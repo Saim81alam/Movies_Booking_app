5601bfd302d81efe74071ba67997a4fab39202fe
Saim81alam <s.a20198171@gmail.com>
2025-02-07 00:56:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+// import "./App.css";
+import MoviesPage from "./commponent/MoviesPage";
+import MoviesDetails from "./commponent/MoviesDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Form from "./commponent/Form";
+
+function App() {
+  const MoviesList = [
+    {
+      id: 1,
+      title: "KGF CHAPTER 1",
+      image:
+        "https://m.media-amazon.com/images/S/pv-target-images/149ac783c0e32bfecd62d2d447b5c36d304d5a5afe30db4d701dc93dc8bba5b0.jpg",
+      details:
+        "Rocky's Mother: If you gain courage because a thousand people are standing behind you, then you can win only one war.",
+    },
+    {
+      id: 2,
+      title: "KGF CHAPTER 2",
+      image: "https://static.toiimg.com/photo/90518208.cms",
+      details:
+        "Rocky : Violence, Violence, Violence, I don't like it, I Avoid... . but Violence likes me, I can't Avoid.",
+    },
+    {
+      id: 3,
+      title: "PROJECT 24",
+      image:
+        "https://www.hindustantimes.com/ht-img/img/2023/02/18/1600x900/Project_K_1676700708297_1676700708506_1676700708506.jpg",
+      details: "in this movie a scientist make time travelling watch.",
+    },
+    {
+      id: 4,
+      title: "MARCO",
+      image: "https://i.ytimg.com/vi/RWGAOhzQDz0/maxresdefault.jpg",
+      details:
+        "The film received well critical acclaim from both critics and audience alike, and emerged as the highest-grossing A-rated Malayalam film of all-time,",
+    },
+    {
+      id: 5,
+      title: "LEO",
+      image:
+        "https://occ-0-8407-1361.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABfAjTWV_un8QDeuY6BOHfenHF5tL3Jf4e9HiVapqaznk2WmVuB36GxTCCIOvjKgPMi9EVm7YhFwA2_rn7f8sMPhJdC_O1rBG57ka.jpg?r=c57",
+      details:
+        "The film received well critical acclaim from both critics and audience alike, and emerged as the highest-grossing A-rated Malayalam film of all-time,",
+    },
+    {
+      id: 6,
+      title: "BAHUBALI",
+      image:
+        "https://wallpapers.com/images/hd/prabhas-in-bahubali-ik4trnhyq3dgv91h.jpg",
+      details:
+        "The film received well critical acclaim from both critics and audience alike, and emerged as the highest-grossing A-rated Malayalam film of all-time,",
+    },
+    {
+      id: 7,
+      title: "KALKI",
+      image: "https://i.ytimg.com/vi/y1-w1kUGuz8/maxresdefault.jpg",
+      details:
+        "The film received well critical acclaim from both critics and audience alike, and emerged as the highest-grossing A-rated Malayalam film of all-time,",
+    },
+    {
+      id: 8,
+      title: "KAALA",
+      image:
+        "https://c4.wallpaperflare.com/wallpaper/952/216/876/hindi-kaala-karikaalan-rajnikanth-telugu-wallpaper-preview.jpg",
+      details:
+        "The film received well critical acclaim from both critics and audience alike, and emerged as the highest-grossing A-rated Malayalam film of all-time,",
+    },
+  ];
+
+  return (
+    <center>
+      {" "}
+      <h1>Movies Booking</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MoviesPage MoviesList={MoviesList} />} />
+          <Route path="/next" element={<MoviesDetails />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </Router>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/commponent/Form.jsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+// import "./form.css";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function Form() {
+  const [btnclick, setBtnClick] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [randomNumber, setRandomNumber] = useState();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { title, image } = location.state || {};
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    mob: "",
+  });
+  const [inputValue, setInputValue] = useState({
+    fullname: "",
+    email: "",
+    mob: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newrandomNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+    setRandomNumber(newrandomNumber);
+
+    if (
+      inputValue.fullname != "" &&
+      inputValue.email != "" &&
+      inputValue.mob != ""
+    ) {
+      setFormData({
+        fullname: inputValue.fullname,
+        email: inputValue.email,
+        mob: inputValue.mob,
+      });
+      setBtnClick(!btnclick);
+      alert(`Congratulation Your ticket Number is ${newrandomNumber}`);
+    } else alert("palese fill all details");
+
+    setInputValue({
+      fullname: "",
+      email: "",
+      mob: "",
+    });
+  };
+
+  const showTicket = (e) => {
+    e.preventDefault();
+    setShowBtn(!showBtn);
+    e.target.innerText = showBtn
+      ? "SHOW TICKET"
+      : "TAKE A PICTURE OF THIS OR SCREENSHOT";
+  };
+
+  console.log(formData);
+
+  return (
+    <>
+      {!btnclick && (
+        <div
+          className="card container p-3 text-start mb-2 d-flex "
+          style={{ width: "22rem", height: "20rem" }}
+        >
+          <h3 className="m-auto">Fill Details</h3>
+          <form
+            onSubmit={handleSubmit}
+            className="d-flex flex-column justify-content-center"
+          >
+            <div className="mb-3 mt-2 d-flex flex-row justify-content-between">
+              <label htmlFor="fullname" className="form-label">
+                Full Name:
+              </label>
+              <input
+                type="text"
+                id="fullname"
+                name="fullname"
+                value={inputValue.fullname}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3 mt-2 d-flex flex-row justify-content-between ">
+              <label htmlFor="email" className="form-label">
+                Email address:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={inputValue.email}
+                aria-describedby="emailHelp"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3 mt-2 d-flex flex-row justify-content-between">
+              <label htmlFor="mob" className="form-label">
+                Mobile no :
+              </label>
+              <input
+                type="tel"
+                id="mob"
+                name="mob"
+                maxLength="10"
+                value={inputValue.mob}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="d-flex justify-content-around">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                style={{ width: "7rem" }}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      {btnclick && (
+        <button type="text" className="btn btn-success" onClick={showTicket}>
+          SHOW TICKET
+        </button>
+      )}
+
+      {/* ticket confrom */}
+
+      {showBtn && (
+        <div
+          className="card mb-3 mt-3"
+          style={{ width: "30rem", height: "15rem:" }}
+          id="ticket"
+        >
+          <div className="row g-0">
+            <div className="col-md-4 p-2 rounded">
+              <div
+                className="mt-4 mb-2"
+                style={{
+                  height: "8rem",
+                  width: "auto",
+                  background: `url(${image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+              <h5 className="card-title">{title}</h5>
+            </div>
+            <div className="col-md-8 p-3">
+              <div className="card-body text-start">
+                <p>
+                  <span className="fw-bold">TICKET NO :</span>
+                  {randomNumber}
+                </p>
+                <p>
+                  <span className="fw-bold">FULL Name : </span>
+                  {formData.fullname}
+                </p>
+                <p>
+                  <span className="fw-bold">Email : </span>
+                  {formData.email}
+                </p>
+                <p>
+                  <span className="fw-bold">MOB : </span>
+                  {formData.mob}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <button
+        type="back"
+        className="btn btn-danger ms-2"
+        style={{ width: "10rem" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go back to Home
+      </button>
+    </>
+  );
+}
+
+export default Form;
